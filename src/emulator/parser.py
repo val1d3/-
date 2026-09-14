998e26583aba45b82ac09bf1194170154e184daa
val1d3 <vitalik20022008@gmail.com>
2026-09-14 22:46:44 +0300

    Update parser.py

--- src/emulator/parser.py
+++ src/emulator/parser.py
@@ -1,1 +1,27 @@
+"""Разбор строк, введённых пользователем, на команду и аргументы."""
 
+import shlex
+
+class ParserError(Exception):
+    pass
+
+def parse_line(line):
+    line = line.strip() #стирка ненужных пробелов по краям
+
+    if line == "":
+        return [] 
+      
+    try:
+        fullslova = shlex.split(line)
+    except ValueError:
+        raise ParserError("не закрыта кавычка в команде")
+    return fullslova
+
+def split_command(fullslova):
+    if len(fullslova) == 0:
+        return "", []
+    command = fullslova[0]
+    args = []
+    for i in range(1, len(fullslova)):
+        args.append(fullslova[i])
+    return command, args
