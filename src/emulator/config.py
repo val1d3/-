65d6a0054fff57d35cf584c11167a3498acc0ee8
val1d3 <vitalik20022008@gmail.com>
2026-09-21 11:01:38 +0300

    fix(config): имя VFS вместо заглушки no-vfs

--- src/emulator/config.py
+++ src/emulator/config.py
@@ -6,7 +6,7 @@ class EmulatorConfig:
 
     def __init__(self, vfs_path=None):
         self.vfs_path = vfs_path
-        self.vfs_name = "VFS"  # имя VFS, пока просто зафиксировано в коде
+        self.vfs_name = "VFS"  
 
     def get_vfs_name(self):
         """Вернуть имя VFS для показа в заголовке окна."""
